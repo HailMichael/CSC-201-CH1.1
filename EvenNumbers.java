7068e66dff1fe132da13f656ad72a1ad3edf0df2
Michael Hailemariam <MichaelHailemariam@Michaels-MacBook-Pro.local>
2018-01-17 01:41:41 -0500

    first commit

---
+++ EvenNumbers.java
@@ -0,0 +1,28 @@
+public class EvenNumbers {
+
+
+
+
+	public static void main(String[] args) {
+		
+		
+		for (int i=0; i<200; i++) {
+
+		int ii = 0;
+
+		do {
+
+			ii++;
+
+			ii++;
+
+			System.out.print(ii + " ");
+
+								
+
+		} while (ii<200);
+
+		System.out.print("\n");
+		}
+	}
+}	
---
+++ OddNumbers.java
@@ -0,0 +1,16 @@
+public class OddNumbers {
+
+
+
+
+	public static void main(String[] args) {
+
+		for (int i=0; i<200; i++) {
+
+			i++;
+
+			System.out.print(i + " ");
+
+		}			
+	}
+}
